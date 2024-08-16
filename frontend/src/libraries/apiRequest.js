ade79c415f9609e70db167bf7cf0002d40cc6c45
tijothomas2000 <tijonarithookil@gmail.com>
2024-08-16 22:12:11 +0530

    Pointed to urls

--- frontend/src/libraries/apiRequest.js
+++ frontend/src/libraries/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL:"http://localhost:8800/api",
+    baseURL:"https://realestate-backend-mern.vercel.app/api",
     withCredentials: true,
 })
 
