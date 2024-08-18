3c4804f85d238c5efeee0c79354c005e832d3eef
tijothomas2000 <tijonarithookil@gmail.com>
2024-08-18 12:52:32 +0530

    frontend modified

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, methods: 'GET,POST,PUT,DELETE', allowedHeaders: 'Content-Type, Authorization', credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, methods: 'GET,POST,PUT,DELETE,UPDATE', allowedHeaders: 'Content-Type, Authorization', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -56,7 +56,7 @@ export const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: age,
-            secure: true
+            // secure: true
         }).status(200).json(userInfo);
 
     } catch (err) {
