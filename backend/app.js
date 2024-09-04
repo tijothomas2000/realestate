2ee8e5a5b586ee2306c3cf54c35ba7190ba247ee
tijothomas2000 <tijonarithookil@gmail.com>
2024-09-04 13:13:38 +0530

    cors modified

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8800;
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, methods: 'GET, PUT, POST, DELETE', allowedHeaders: 'Content-Type, Authorization', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
