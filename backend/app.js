dd9bfa5a267b21925f22f063408f4eac08f128f3
tijothomas2000 <tijonarithookil@gmail.com>
2024-09-04 12:37:23 +0530

    Backend modified

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8800;
 
-app.use(cors({ origin: process.env.CLIENT_URL, methods: 'GET,POST,PUT,DELETE,UPDATE', allowedHeaders: 'Content-Type, Authorization', credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, allowedHeaders: 'Content-Type, Authorization', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
